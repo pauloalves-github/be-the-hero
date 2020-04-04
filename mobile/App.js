0eb9ebe747dd0953172a9f64cdb6df50ed897e40
paulo alves <pauloalves.ti@outlook.com>
2020-04-04 15:50:00 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+//import { setLightEstimationEnabled } from 'expo/build/AR';
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
